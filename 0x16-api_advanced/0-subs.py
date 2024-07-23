215ba07c18107c345fe1c0f70f5d2864a90410ab
A-asava <adikasaid996@gmail.com>
2024-07-23 13:21:56 +0000

    final_updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,17 +10,15 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            subscribers = data['data']['subscribers']
+            subscribers = data.get('data', {}).get('subscribers', 0)
             return subscribers
         else:
             return 0
-    except requests.exceptions.RequestException as e:
-        return 0
-    except ValueError:
+    except requests.RequestException:
         return 0
 
