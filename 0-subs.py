5686b13e42505b14f99d069b6ee3b46c5c76134e
A-asava <adikasaid996@gmail.com>
2024-07-23 14:51:04 +0000

    webstack_monitoring

---
+++ 0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""returns total number of subscribers"""
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns
+        total number of subscribers
+    """
+    import requests
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My-User-Agent'}
+    response = requests.get(url, headers=headers)
+    if response.status_code >= 300:
+        return 0
+    data = response.json()
+    return data.get('data').get('subscribers')
